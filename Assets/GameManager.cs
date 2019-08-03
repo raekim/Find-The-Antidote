302c5b06376672ed1a7450d2a79428117395e11e
raekim <raekim95@gmail.com>
2019-08-03 09:22:10 +0900

    character move 1unit by keyboard arrow input

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int pixelsPerUnit;
+
+    static GameManager _instance;
+    static public GameManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public int GetPixelsPerUnit()
+    {
+        return pixelsPerUnit;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
