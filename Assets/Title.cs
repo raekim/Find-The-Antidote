55ae0c22d42049e328969f9c712e4755fc92477f
raekim <raekim95@gmail.com>
2019-08-04 23:58:52 +0900

    Game complete

---
+++ Assets/Title.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField] GameObject explosioinEffect;
+    [SerializeField] Transform explosionPosition;
+
+    bool gameStarted = false;
+    float startTime;
+    float waitTime = 0.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartInPut();
+        WaitAndStart();
+    }
+
+    private void StartInPut()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Instantiate(explosioinEffect, explosionPosition.position + new Vector3(0,0,-1), Quaternion.identity);
+            gameStarted = true;     
+        }
+    }
+
+    void WaitAndStart()
+    {
+        if (gameStarted)
+        {
+            startTime += Time.deltaTime;
+            if(startTime >= waitTime)
+            {
+                // load the next scene in build order
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+            
+    }
+}
